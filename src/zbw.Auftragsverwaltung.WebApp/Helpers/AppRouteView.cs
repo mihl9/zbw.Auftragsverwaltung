9e57e3ee900c610aa44abaf732fd39dc802a4aee
Michael Huber <michael.huber@huberit.ch>
2021-09-05 19:35:24 +0200

    fix: write correct namespace

--- src/zbw.Auftragsverwaltung.WebApp/Helpers/AppRouteView.cs
+++ src/zbw.Auftragsverwaltung.WebApp/Helpers/AppRouteView.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
+using zbw.Auftragsverwaltung.WebApp.Services;
 
 namespace zbw.Auftragsverwaltung.WebApp.Helpers
 {
