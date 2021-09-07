bcb397cfdb407d5072b49d46e9c035a7e987b128
Michael Huber <michael.huber@huberit.ch>
2021-09-07 22:07:55 +0200

    feat: add component Alert
    - Create Component folder for future components
    - Add Alert component
    - Define a HihlightAlert enumeration for highlighting components

---
+++ src/zbw.Auftragsverwaltung.BlazorApp/Components/Alert.razor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using zbw.Auftragsverwaltung.BlazorApp.Helpers;
+
+namespace zbw.Auftragsverwaltung.BlazorApp.Components
+{
+    public partial class Alert
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        [Parameter]
+        public HighlightLevel Level { get; set; }
+
+        [Parameter]
+        public bool Dismissible { get; set; }
+
+        [Parameter]
+        public bool Visible { get; set; }
+
+        public void OnDismissClick()
+        {
+            Visible = false;
+            StateHasChanged();
+        }
+
+        public void Show(string message, HighlightLevel alertLevel)
+        {
+            Level = alertLevel;
+            ChildContent = builder => builder.AddContent(0, message);
+            Visible = true;
+            StateHasChanged();
+        }
+
+        public void Close()
+        {
+            OnDismissClick();
+        }
+
+	}
+}
---
+++ src/zbw.Auftragsverwaltung.BlazorApp/Helpers/AlertLevel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace zbw.Auftragsverwaltung.BlazorApp.Helpers
+{
+    public class HighlightLevel
+    {
+        public static HighlightLevel Primary = new HighlightLevel("primary");
+        public static HighlightLevel Info = new HighlightLevel("info");
+        public static HighlightLevel Success = new HighlightLevel("success");
+        public static HighlightLevel Danger = new HighlightLevel("danger");
+        public static HighlightLevel Warning = new HighlightLevel("warning");
+        public static HighlightLevel Default = new HighlightLevel("default");
+
+        public string Level { get; set; }
+
+        public HighlightLevel(string level)
+        {
+            Level = level;
+        }
+
+	}
+}
