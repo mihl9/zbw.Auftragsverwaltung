94af59a4cdf04b7804e6ff255a45fda704db4300
Michael Huber <michael.huber@huberit.ch>
2021-09-07 22:12:56 +0200

    feat: add badge component
    - Add badge logic
    - Add html for the Alert

---
+++ src/zbw.Auftragsverwaltung.BlazorApp/Components/Badge.razor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using zbw.Auftragsverwaltung.BlazorApp.Helpers;
+
+namespace zbw.Auftragsverwaltung.BlazorApp.Components
+{
+    public partial class Badge
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        [Parameter]
+        public HighlightLevel Level { get; set; }
+
+    }
+}
