78f9953f5e1c97ee2691bf550d591defd821c363
Michael Huber <michael.huber@huberit.ch>
2021-09-07 22:20:08 +0200

    feat: add card component

---
+++ src/zbw.Auftragsverwaltung.BlazorApp/Components/Card.razor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace zbw.Auftragsverwaltung.BlazorApp.Components
+{
+    public partial class Card
+    {
+        [Parameter]
+        public string MaterialIcon { get; set; }
+
+        [Parameter]
+        public RenderFragment Title { get; set; }
+
+        [Parameter]
+        public RenderFragment Body { get; set; }
+
+        [Parameter]
+        public RenderFragment Footer { get; set; }
+
+        public string GetCardClasses()
+        {
+            var c = string.Empty;
+
+            if (!string.IsNullOrEmpty(MaterialIcon))
+            {
+                c += "card-header-icon";
+            }
+            else
+            {
+                c += "card-header-text";
+            }
+
+            return c;
+        }
+
+	}
+}
