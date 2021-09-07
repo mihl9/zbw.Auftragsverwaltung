367a604657a875ffd9588e943325322a1c01fb17
Michael Huber <michael.huber@huberit.ch>
2021-09-08 00:28:13 +0200

    feat: Add File Container as a file wrapper

---
+++ src/zbw.Auftragsverwaltung.Domain/Common/FileContainer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace zbw.Auftragsverwaltung.Domain.Common
+{
+    public class FileContainer
+    {
+        public string ContentType { get; set; }
+        public string FileName { get; set; }
+        public Stream Content { get; set; }
+
+    }
+}
