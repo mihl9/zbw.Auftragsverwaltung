85a382290291fb821d6cf1330dfa8d2dfc22a8c6
Michael Huber <michael.huber@huberit.ch>
2021-09-08 00:37:41 +0200

    feat: add sub component for the table component
    - Selector for the tablesize

---
+++ src/zbw.Auftragsverwaltung.BlazorApp/Components/Table/TableSizeSelector.razor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace zbw.Auftragsverwaltung.BlazorApp.Components.Table
+{
+    public partial class TableSizeSelector : IDisposable
+    {
+        [Parameter]
+        public int Value { get; set; }
+
+        [Parameter]
+        public EventCallback<ChangeEventArgs> OnChange { get; set; }
+
+        [Parameter]
+        public Dictionary<int, string> AvailableSelections { get; set; } = new Dictionary<int, string>()
+        {
+            {5, "5" },
+            {10, "10" },
+            {25, "25" },
+            {50, "50" },
+            {-1, "All" }
+        };
+
+        private void OnCountChange(ChangeEventArgs e)
+        {
+            if (!int.TryParse(e.Value.ToString(), out var val)) return;
+
+            Value = val;
+            NotifyOnChange(e);
+        }
+
+        private async void NotifyOnChange(ChangeEventArgs e) => await OnChange.InvokeAsync(e);
+
+        public void Dispose()
+        {
+            OnChange = EventCallback<ChangeEventArgs>.Empty;
+        }
+    }
+}
