fd1b4967fb7d81049b9995b3189b48e75b12d1b4
Michael Huber <michael.huber@huberit.ch>
2021-09-07 22:26:09 +0200

    feat: add Component to dynamically load content

---
+++ src/zbw.Auftragsverwaltung.BlazorApp/Components/DynamicContentLoader.razor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace zbw.Auftragsverwaltung.BlazorApp.Components
+{
+    public partial class DynamicContentLoader
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        [Parameter]
+        public bool HideContent { get; set; }
+
+    }
+}
