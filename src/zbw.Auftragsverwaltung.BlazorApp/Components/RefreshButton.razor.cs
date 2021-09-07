2d8390cd048dead9966edd722b8f70151d4b0623
Michael Huber <michael.huber@huberit.ch>
2021-09-08 00:27:53 +0200

    feat: Add refresh component

---
+++ src/zbw.Auftragsverwaltung.BlazorApp/Components/RefreshButton.razor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace zbw.Auftragsverwaltung.BlazorApp.Components
+{
+    public partial class RefreshButton : IDisposable
+    {
+        private const string SpinningClass = "fa-spin";
+
+        private async Task HandleRefreshClick()
+        {
+            await NotifyRefresh();
+        }
+
+        private void OnMouseIn(MouseEventArgs e)
+        {
+            Animation = SpinningClass;
+        }
+
+        private void OnMouseOut(MouseEventArgs e)
+        {
+            Animation = string.Empty;
+        }
+
+        [Parameter]
+        public EventCallback OnRefresh { get; set; }
+
+        private string Animation { get; set; }
+
+        private async Task NotifyRefresh() => await OnRefresh.InvokeAsync(this);
+
+        public void Dispose()
+        {
+            OnRefresh = EventCallback.Empty;
+        }
+
+	}
+}
