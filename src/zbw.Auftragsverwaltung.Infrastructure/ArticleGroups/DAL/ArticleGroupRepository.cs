159c35a900717da4c07dc9e887b34e3b1f6032ee
Simon Scherer <simon_scherer@bluewin.ch>
2021-09-08 17:31:26 +0200

    change sqlCommand

--- src/zbw.Auftragsverwaltung.Infrastructure/ArticleGroups/DAL/ArticleGroupRepository.cs
+++ src/zbw.Auftragsverwaltung.Infrastructure/ArticleGroups/DAL/ArticleGroupRepository.cs
@@ -11,7 +11,7 @@ namespace zbw.Auftragsverwaltung.Infrastructure.ArticleGroups.DAL
 {
     public class ArticleGroupRepository : BaseRepository<ArticleGroup, Guid, OrderManagementContext>, IArticleGroupRepository
     {
-        private const string CTE_SQL_COMMAND = "WITH CTE_ARTICLEGROUPS (Id, ArticleGroupId ) AS (SELECT	Id , ArticlegroupId FROM dbo.ArticleGroups WHERE ArticlegroupId IS NULL UNION ALL SELECT pn.Id, pn.ArticlegroupId FROM dbo.ArticleGroups AS pn INNER JOIN CTE_ARTICLEGROUPS AS p1 ON p1.Id = pn.ArticlegroupId) SELECT	Id, ArticlegroupId FROM CTE_ARTICLEGROUPS ORDER BY ArticlegroupId";
+        private const string CTE_SQL_COMMAND = "WITH CTE_ARTICLEGROUPS (Id, ArticleGroupName, ArticleGroupId ) AS (SELECT Id, Name, ArticlegroupId FROM dbo.ArticleGroups WHERE ArticlegroupId IS NULL UNION ALL SELECT pn.Id,pn.Name, pn.ArticlegroupId FROM dbo.ArticleGroups AS pn INNER JOIN CTE_ARTICLEGROUPS AS p1 ON p1.Id = pn.ArticlegroupId) SELECT	Id,ArticleGroupName, ArticlegroupId FROM CTE_ARTICLEGROUPS ORDER BY ArticlegroupId";
 
         public ArticleGroupRepository(OrderManagementContext dbContext) : base(dbContext)
         {
