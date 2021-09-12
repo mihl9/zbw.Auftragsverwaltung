5c2aa123c0d0e18eeecb93a6c33c711d3016558b
Simon Scherer <simon_scherer@bluewin.ch>
2021-09-12 22:52:49 +0200

    test: Add Test for Invoice & Address

---
+++ tests/zbw.Auftragsverwaltung.Core.Test/Addresses/AdressRepositoryHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Moq;
+using zbw.Auftragsverwaltung.Core.Addresses.Contracts;
+using zbw.Auftragsverwaltung.Core.Addresses.Entities;
+using zbw.Auftragsverwaltung.Core.ArticleGroups.Contracts;
+using zbw.Auftragsverwaltung.Core.ArticleGroups.Entities;
+
+namespace zbw.Auftragsverwaltung.Core.Test.Addresses
+{
+    public class AdressRepositoryHelper
+    {
+        public static Mock<IAddressRepository> TestAdressRepository(IList<Address> address)
+        {
+            var repo = new Mock<IAddressRepository>();
+
+            repo.Setup(x => x.DeleteAsync(It.IsAny<Address>())).ReturnsAsync(true).Callback<Address>(x => address.Remove(x));
+            repo.Setup(x => x.UpdateAsync(It.IsAny<Address>())).ReturnsAsync(true);
+            repo.Setup(x => x.AddAsync(It.IsAny<Address>())).ReturnsAsync((Address c) => c).Callback<Address>( address.Add);
+            repo.Setup(x => x.GetByIdAsync(It.IsAny<Guid>()))
+                .ReturnsAsync((Guid id) => address.First(x => x.Id.Equals(id)));
+
+            return repo;
+        }
+    }
+}
--- tests/zbw.Auftragsverwaltung.Core.Test/Invoices/InvoiceBllTest.cs
+++ tests/zbw.Auftragsverwaltung.Core.Test/Invoices/InvoiceBllTest.cs
@@ -19,29 +19,62 @@ using zbw.Auftragsverwaltung.Core.Customers.BLL;
 using zbw.Auftragsverwaltung.Core.Customers.Contracts;
 using zbw.Auftragsverwaltung.Core.Customers.Entities;
 using zbw.Auftragsverwaltung.Core.Customers.Profiles;
+using zbw.Auftragsverwaltung.Core.Invoices.BLL;
+using zbw.Auftragsverwaltung.Core.Invoices.Contracts;
+using zbw.Auftragsverwaltung.Core.Invoices.Entities;
+using zbw.Auftragsverwaltung.Core.Invoices.Profiles;
 using zbw.Auftragsverwaltung.Core.Test.Customers;
+using zbw.Auftragsverwaltung.Core.Test.DataTransfer;
 using zbw.Auftragsverwaltung.Core.Test.Helpers;
 using zbw.Auftragsverwaltung.Core.Test.Users;
 using zbw.Auftragsverwaltung.Core.Users.Entities;
 using zbw.Auftragsverwaltung.Core.Users.Enumerations;
 using zbw.Auftragsverwaltung.Domain.Addresses;
+using zbw.Auftragsverwaltung.Domain.Invoices;
 
-namespace zbw.Auftragsverwaltung.Core.Test.Addresses
+namespace zbw.Auftragsverwaltung.Core.Test.Invoices
 {
     public class InvoiceBllTest
     {
         private readonly AddressBll _address;
+        private readonly InvoiceBll _invoice;
         private readonly CustomerBll _customer;
         private readonly Mock<IAddressRepository> _addressRepository;
         private readonly Mock<ICustomerRepository> _customerRepository;
+        private readonly Mock<IInvoiceRepository> _invoiceRepository;
         private readonly Mock<UserManager<User>> _userManager;
 
+        private readonly List<Invoice> _invoices = new List<Invoice>()
+        {
+            new Invoice
+            {
+                Id = GuidCollection.Id001,
+                AddressId =  GuidCollection.Id005,
+                Date = new DateTime(2020,5,5),
+                Number = 1,
+                Netto = 90,
+                Brutto = 100,
+                Tax = 10,
+                AdressValidFrom = new DateTime(2020,1,1)
+            },
+            new Invoice
+            {
+                Id = GuidCollection.Id002,
+                AddressId =  GuidCollection.Id003,
+                Date = new DateTime(2020,5,5),
+                Number = 2,
+                Netto = 45,
+                Brutto = 50,
+                Tax = 10,
+                AdressValidFrom = new DateTime(2020,1,1)
+            }
+        };
 
         private readonly List<Address> _addresses = new List<Address>()
         {
             new Address
             {
-                Id = GuidCollection.Id001,
+                Id = GuidCollection.Id005,
                 CustomerId = GuidCollection.Id002,
                 Street = "Musterstrasse",
                 Number = 5,
@@ -65,6 +98,7 @@ namespace zbw.Auftragsverwaltung.Core.Test.Addresses
             }
         };
 
+        
         private readonly List<User> _users = new List<User>()
         {
             new User()
@@ -84,17 +118,19 @@ namespace zbw.Auftragsverwaltung.Core.Test.Addresses
             new Customer(){CustomerNr = 1, Firstname = "Michael", Lastname = "Huber", Website = string.Empty, UserId = GuidCollection.Id001, Id = GuidCollection.Id001},
             new Customer(){CustomerNr = 2, Firstname = "Test", Lastname = "Test", Website = string.Empty, UserId = GuidCollection.Id002, Id = GuidCollection.Id002}
         };
-
+        
         public InvoiceBllTest()
         {
             var configuration = new MapperConfiguration(cfg =>
             {
                 cfg.AddProfile(new AddressProfile());
                 cfg.AddProfile(new CustomerProfile());
+                cfg.AddProfile(new InvoiceProfile());
             });
             IMapper mapper = new Mapper(configuration);
 
-            _addressRepository = AdressRepositoryHelper.TestAdressRepository(_addresses);
+            _invoiceRepository = InvoiceRepositoryHelper.TestInvoiceRepository(_invoices);
+            _addressRepository = AddressRepositoryHelper.TestAddressRepository(_addresses);
             _userManager = UserManagerTestHelper.TestUserManager<User>(_users);
             _customerRepository = CustomerRepositoryHelper.TestCustomerRepository(_customers);
 
@@ -113,72 +149,70 @@ namespace zbw.Auftragsverwaltung.Core.Test.Addresses
                 return _users.First(x => x.Id.ToString().Equals(id));
             });
 
+            _invoice = new InvoiceBll(mapper, _invoiceRepository.Object);
             _customer = new CustomerBll(_customerRepository.Object, mapper, _userManager.Object);
             _address = new AddressBll(_addressRepository.Object,mapper,_userManager.Object,_customer);
         }
 
         [Fact]
-        public void Get_Address_As_Administrator_Not_Throw()
+        public void Get_Invoice_Not_Throw()
         {
-            Func<Task> get = async () => { await _address.Get(GuidCollection.Id001, GuidCollection.Id001); };
+            Func<Task> get = async () => { await _invoice.Get(GuidCollection.Id001); };
             get.Should().NotThrow<Exception>();
         }
         [Fact]
-        public void Delete_Address_Not_Throw()
+        public void Delete_Invoice_Not_Throw()
         {
-            var addressDto = new AddressDto
+            var invoiceDto = new InvoiceDto()
             {
-                Id = GuidCollection.Id004,
-                CustomerId = GuidCollection.Id002,
-                Street = "Musterstrasse",
-                Number = 5,
-                Zip = "9000",
-                Recipient = "Musterstrasse 5, 9000 St.Gallen",
-                Location = "St. Gallen",
-                ValidFrom = new DateTime(2020,5,5),
-                ValidTo = null
+                Id = GuidCollection.Id001,
+                AddressId =  GuidCollection.Id005,
+                Date = new DateTime(2020,5,5),
+                Number = 1,
+                Netto = 90,
+                Brutto = 100,
+                Tax = 10,
+                AdressValidFrom = new DateTime(2020,1,1)
             };
-            Func<Task> delete = async () => { await _address.Delete(addressDto, GuidCollection.Id001); };
+            Func<Task> delete = async () => { await _invoice.Delete(invoiceDto); };
             delete.Should().NotThrow<Exception>();
         }
 
         [Fact]
-        public void Add_Address_Not_Null()
+        public void Add_Invoice_Not_Null()
         {
-            var addressDto = new AddressDto
+            var invoiceDto = new InvoiceDto()
             {
-                Id = GuidCollection.Id004,
-                CustomerId = GuidCollection.Id002,
-                Street = "Musterstrasse",
-                Number = 5,
-                Zip = "9000",
-                Recipient = "Musterstrasse 5, 9000 St.Gallen",
-                Location = "St. Gallen",
-                ValidFrom = new DateTime(2020,5,5),
-                ValidTo = null
+                Id = GuidCollection.Id007,
+                AddressId =  GuidCollection.Id005,
+                Date = new DateTime(2020,5,5),
+                Number = 1,
+                Netto = 90,
+                Brutto = 100,
+                Tax = 10,
+                AdressValidFrom = new DateTime(2020,1,1)
             };
-            Func<Task> add = async () => { await _address.Add(addressDto, GuidCollection.Id001); };
+            Func<Task> add = async () => { await _invoice.Add(invoiceDto); };
             add.Should().NotThrow<Exception>();
-            Func<Task> get = async () => { await _address.Get(GuidCollection.Id004,GuidCollection.Id001); };
+            Func<Task> get = async () => { await _invoice.Get(GuidCollection.Id007); };
             get.Should().NotBeNull();
         }
 
         [Fact]
-        public void Update_Address_Not_Null()
+        public void Update_Invoice_Not_Null()
         {
-            var addressDto = new AddressDto
+            var invoiceDto = new InvoiceDto()
             {
                 Id = GuidCollection.Id001,
-                CustomerId = GuidCollection.Id002,
-                Street = "Musterstrasse",
-                Number = 5,
-                Zip = "9000",
-                Recipient = "Beispielstrasse 5, 9000 St.Gallen",
-                Location = "St. Gallen",
-                ValidFrom = new DateTime(2020,5,5),
-                ValidTo = null
+                AddressId =  GuidCollection.Id005,
+                Date = new DateTime(2020,10,5),
+                Number = 1,
+                Netto = 90,
+                Brutto = 100,
+                Tax = 10,
+                AdressValidFrom = new DateTime(2020,1,1)
             };
-            Func<Task> update = async () => { await _address.Update(addressDto,GuidCollection.Id002); };
+            Func<Task> update = async () => { await _invoice.Update(invoiceDto); };
             update.Should().NotThrow<Exception>();
             update.Should().NotBeNull();
         }
